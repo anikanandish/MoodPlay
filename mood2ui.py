facf94133396f25e7d0de3ddb9a90570f46ee2df
anikanandish <anikanandish@gmail.com>
2026-08-03 22:40:37 +0530

    bleh

--- mood2ui.py
+++ mood2ui.py
@@ -18,7 +18,7 @@ mood_history = []
 
 
 def log_mood(mood):
-    """Saves recent mood queries to history (keeps last 5 only)."""
+    """Saves recent mood queries to history (keeps last 3 only)."""
     if mood and mood not in mood_history:
         mood_history.insert(0, mood)
         if len(mood_history) > 3:
